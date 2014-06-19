f135651fbab12f1db1123337adb5892bf9b4aa2e
ebeneventano <emmanuel.beneventano@gmail.com>
2014-06-19 19:35:00 -0300

    Agrego atributos, getters and setters

--- src/untref/aydoo/dtos/ExportYmlDTO.java
+++ src/untref/aydoo/dtos/ExportYmlDTO.java
@@ -1,5 +1,35 @@
 package untref.aydoo.dtos;
 
 public class ExportYmlDTO {
-
+	
+	private Integer cantidadVecesUsada;
+	private Integer promedioUso;
+	private Integer mayorRecorridoRealizado;
+	private Integer cantidadMayorRecorridoRealizado;
+	
+	public Integer getCantidadVecesUsada() {
+		return cantidadVecesUsada;
+	}
+	public void setCantidadVecesUsada(Integer cantidadVecesUsada) {
+		this.cantidadVecesUsada = cantidadVecesUsada;
+	}
+	public Integer getPromedioUso() {
+		return promedioUso;
+	}
+	public void setPromedioUso(Integer promedioUso) {
+		this.promedioUso = promedioUso;
+	}
+	public Integer getMayorRecorridoRealizado() {
+		return mayorRecorridoRealizado;
+	}
+	public void setMayorRecorridoRealizado(Integer mayorRecorridoRealizado) {
+		this.mayorRecorridoRealizado = mayorRecorridoRealizado;
+	}
+	public Integer getCantidadMayorRecorridoRealizado() {
+		return cantidadMayorRecorridoRealizado;
+	}
+	public void setCantidadMayorRecorridoRealizado(
+			Integer cantidadMayorRecorridoRealizado) {
+		this.cantidadMayorRecorridoRealizado = cantidadMayorRecorridoRealizado;
+	}
 }
