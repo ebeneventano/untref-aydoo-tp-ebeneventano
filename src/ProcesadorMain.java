4124e04b6fcd0907af1f9258f80221bb591f840a
ebeneventano <emmanuel.beneventano@gmail.com>
2014-06-22 01:12:51 -0300

    Agrego "threads"

--- src/ProcesadorMain.java
+++ src/ProcesadorMain.java
@@ -12,20 +12,46 @@ public class ProcesadorMain {
 	
 	
 	public static void main(String[] args) throws IOException {
-		ProcesadorEstadistico procesador = new ProcesadorEstadisticoImpl();
 		
-		System.out.println("Indique la carpeta que desea escuchar por nuevos archivos");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String pathAEscuchar = br.readLine();
-		Path pathListening = Paths.get(pathAEscuchar);
 		
-		System.out.println("Indique la carpeta que desea procesar todos los archivos");
-		br = new BufferedReader(new InputStreamReader(System.in));
-		String pathAProcesar = br.readLine();
-		Path pathProcesing = Paths.get(pathAProcesar);
-
-//		procesador.comenzarAEscuchar(pathListening);
-//		procesador.procesarDirectorio(pathProcesing);
+		
+//		Thread t1 = new Thread(new Runnable() {
+//			public void run(){
+//				System.out.println("Indique la carpeta que desea escuchar por nuevos archivos");
+//				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//				String pathAEscuchar;
+//				try {
+//					pathAEscuchar = br.readLine();
+//					Path pathListening = Paths.get(pathAEscuchar);
+//					ProcesadorEstadistico procesador = new ProcesadorEstadisticoImpl();
+//					procesador.comenzarAEscuchar(pathListening);
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
+//		});  
+//		t1.start();
+		
+		
+//		Thread t2 = new Thread(new Runnable() {
+//		     public void run(){
+//		    	 System.out.println("Indique la carpeta que desea procesar todos los archivos");
+//		    	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		    	 br = new BufferedReader(new InputStreamReader(System.in));
+//		    	 String pathAProcesar;
+//				try {
+//					pathAProcesar = br.readLine();
+//					Path pathProcesing = Paths.get(pathAProcesar);
+//					ProcesadorEstadistico procesador = new ProcesadorEstadisticoImpl();
+//					procesador.procesarDirectorio(pathProcesing);
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//		     }
+//		 });  
+//		t2.start();
 		
 	}
 
