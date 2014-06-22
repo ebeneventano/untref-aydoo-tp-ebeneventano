e9c5cff5f010b8abdcd1faad54fd456322e1e7c9
ebeneventano <emmanuel.beneventano@gmail.com>
2014-06-22 18:55:25 -0300

    Actualizo tests

--- test/untref/aydoo/tests/ProcesadorEstadisticoTest.java
+++ test/untref/aydoo/tests/ProcesadorEstadisticoTest.java
@@ -2,16 +2,16 @@ package untref.aydoo.tests;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
+import java.util.Map;
+import java.util.zip.ZipFile;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
 
 import untref.aydoo.dominio.Bicicleta;
-import untref.aydoo.dominio.Recorrido;
+import untref.aydoo.dominio.Trayectoria;
+import untref.aydoo.dtos.DatosBicicleta;
 import untref.aydoo.servicio.ProcesadorEstadistico;
 import untref.aydoo.servicio.ProcesadorEstadisticoImpl;
 
@@ -21,47 +21,78 @@ public class ProcesadorEstadisticoTest {
 	private ProcesadorEstadistico procesador = new ProcesadorEstadisticoImpl();
 	
 	@Test
-	public void comenzarAEscucharNuevoCSVFileEnDirectorio(){
-//		Path folder = Paths.get(System.getProperty("user.home"));
-//		procesador.comenzarAEscuchar(folder);
-	}
-	
-	@Test
-	public void testBicicletaUtilizadaMasVeces(){
-//		String nombreArchivo = "C:" + File.separator + "Users" + File.separator + "Emanuel" + File.separator + "recorrido.csv";
-//		List<Bicicleta> bicicletasMasUsadas = procesador.obtenerBicicletasUtilizadasMasVeces(
-//				procesador.llenarMapaDeBicicletasUsadas(nombreArchivo));
-//
-//		Assert.assertEquals("419", bicicletasMasUsadas.get(0).getId());
-//		Assert.assertEquals("434", bicicletasMasUsadas.get(1).getId());
+	public void testBicicletaUtilizadaMasVeces() throws IOException{
+		String path = new File("src/docs/prueba2010.zip").getCanonicalPath();
+		ZipFile zipFile = new ZipFile(path);
+		Map<Bicicleta, DatosBicicleta> bicicletasMasUsadas = procesador.llenarMapaDeBicicletasUsadas(zipFile);
 
+		bicicletasMasUsadas = procesador.obtenerBicicletasUtilizadasMasVeces(bicicletasMasUsadas);
+		
+		Bicicleta biciMasUsada = new Bicicleta();
+		DatosBicicleta datosBiciMasUsada = new DatosBicicleta();
+		
+		for (Map.Entry<Bicicleta, DatosBicicleta> entry : bicicletasMasUsadas.entrySet())
+		{
+			biciMasUsada = entry.getKey();
+			datosBiciMasUsada = entry.getValue();
+		}
+		
+		Assert.assertEquals("403", biciMasUsada.getId());
+		Assert.assertEquals(Integer.valueOf(4), datosBiciMasUsada.getCantidadVecesUsada());
 	}
 	
 	@Test
-	public void testBicicletaUtilizadaMenosVeces(){
-//		String nombreArchivo = "C:" + File.separator + "Users" + File.separator + "Emanuel" + File.separator + "recorrido.csv";
-//		List<Bicicleta> bicicletasMenosUsadas = procesador.obtenerBicicletaUtilizadaMenosVeces(
-//				procesador.llenarMapaDeBicicletasUsadas(nombreArchivo));
-//		
-//		Assert.assertEquals("452", bicicletasMenosUsadas.get(0).getId());
-//		Assert.assertEquals("464", bicicletasMenosUsadas.get(1).getId());
-	}
-	
-	@Test
-	public void testRecorridoMasRealizado(){
+	public void testBicicletaUtilizadaMenosVeces() throws IOException{
+		String path = new File("src/docs/prueba2010.zip").getCanonicalPath();
+		ZipFile zipFile = new ZipFile(path);
+		Map<Bicicleta, DatosBicicleta> bicicletas = procesador.llenarMapaDeBicicletasUsadas(zipFile);
 
-//		List<Recorrido> recorridosMasRealizados = procesador.obtenerRecorridoMasRealizado();
+		bicicletas = procesador.obtenerBicicletaUtilizadaMenosVeces(bicicletas);
 		
-//		String origenId = recorridosMasRealizados.get(0).getTrayectoria().getEstacionOrigen().getId();
-//		String destinoId = recorridosMasRealizados.get(0).getTrayectoria().getEstacionDestino().getId();
-//
-//		Assert.assertEquals(5, origenId);
-//		Assert.assertEquals(3, destinoId);
+		Bicicleta biciMenosUsada = new Bicicleta();
+		DatosBicicleta datosBiciMenosUsada = new DatosBicicleta();
+		
+		for (Map.Entry<Bicicleta, DatosBicicleta> entry : bicicletas.entrySet())
+		{
+			biciMenosUsada = entry.getKey();
+			datosBiciMenosUsada = entry.getValue();
+		}
+		
+		Assert.assertEquals("351", biciMenosUsada.getId());
+		Assert.assertEquals(Integer.valueOf(1), datosBiciMenosUsada.getCantidadVecesUsada());
+	}
+	
+	@Test
+	public void testRecorridoMasRealizado() throws IOException{
+
+		String path = new File("src/docs/prueba2010.zip").getCanonicalPath();
+		ZipFile zipFile = new ZipFile(path);
+		Map<Bicicleta, DatosBicicleta> bicicletas = procesador.llenarMapaDeBicicletasUsadas(zipFile);
+
+		Map<Trayectoria,Integer> recorridosMasRealizados = procesador.obtenerRecorridoMasRealizado(bicicletas);
+		
+		Trayectoria trayectoria = null;
+		Integer cantidadTrayectoriaMasRealizada = 0;
+		for (Map.Entry<Trayectoria, Integer> entry : recorridosMasRealizados.entrySet())
+		{
+			trayectoria = entry.getKey();
+			cantidadTrayectoriaMasRealizada = entry.getValue();
+		}
+		
+		Assert.assertEquals("7", trayectoria.getEstacionOrigen().getId());
+		Assert.assertEquals("3", trayectoria.getEstacionDestino().getId());
+		Assert.assertEquals(Integer.valueOf(4), cantidadTrayectoriaMasRealizada);
 		
 	}
 	
 	@Test
-	public void testTiempoPromedioDeUso(){
+	public void testTiempoPromedioDeUso() throws IOException{
+		String path = new File("src/docs/prueba2010.zip").getCanonicalPath();
+		ZipFile zipFile = new ZipFile(path);
+		Map<Bicicleta, DatosBicicleta> bicicletas = procesador.llenarMapaDeBicicletasUsadas(zipFile);
+
+		Integer promedio = procesador.getPromedioUso(bicicletas);
+		Assert.assertEquals(Integer.valueOf(19), promedio);
 	}
 	
 	@Test
