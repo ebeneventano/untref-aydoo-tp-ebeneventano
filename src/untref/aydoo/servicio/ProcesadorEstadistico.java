6e01f9ec8098ffc7977213d3423450eead9498e5
ebeneventano <emmanuel.beneventano@gmail.com>
2014-06-22 19:57:10 -0300

    Comentarios en los metodos

--- src/untref/aydoo/servicio/ProcesadorEstadistico.java
+++ src/untref/aydoo/servicio/ProcesadorEstadistico.java
@@ -11,13 +11,47 @@ import untref.aydoo.dominio.Trayectoria;
 import untref.aydoo.dtos.DatosBicicleta;
 
 public interface ProcesadorEstadistico {
-
+	
+	/**
+	 * Pasado un mapa de todas las bicicletas en los distintos csv
+	 * devuelvue un mapa con las bicicletas que se utilizaron mas veces.
+	 */
 	Map<Bicicleta,DatosBicicleta> obtenerBicicletasUtilizadasMasVeces(Map<Bicicleta, DatosBicicleta> bicicletas);
+	
+	/**
+	 * Pasado un mapa de todas las bicicletas en los distintos csv
+	 * devuelvue un mapa con las bicicletas que se utilizaron menos veces.
+	 */
 	Map<Bicicleta,DatosBicicleta> obtenerBicicletaUtilizadaMenosVeces(Map<Bicicleta, DatosBicicleta> bicicletas);
+	
+	/**
+	 * Pasado un mapa de todas las bicicletas en los distintos csv
+	 * devuelvue un mapa con las trayectorias que se utilizaron mas veces.
+	 */
 	Map<Trayectoria, Integer> obtenerRecorridoMasRealizado(Map<Bicicleta, DatosBicicleta> bicicletasEnCsv);
+	
+	/**
+	 * Pasado un path comienza a escuchar por nuevos archivos en ese directorio.
+	 */
 	void comenzarAEscuchar(Path path);
+	
+	/**
+	 * Recorre una sola vez todos los csv involucrados y devuelve un mapa con todas las bicicletas como key
+	 * y como value una entidad DatosBicicletas donde se va calculando distinta informacion necesaria para
+	 * la exportacion.
+	 */
 	Map<Bicicleta,DatosBicicleta> llenarMapaDeBicicletasUsadas(ZipFile zipFile);
+	
+	/**
+	 * Procesa un zip nuevo en un directorio y llama al metodo que llena el mapa de bicicletas para hacer
+	 * luego la exportacion correspondiente
+	 */
 	void procesarCsvEnZip(String nombreArchivo) throws IOException;
+	
+	/**
+	 * Procesa un directorio completo
+	 */
 	void procesarDirectorio(Path pathProcesing) throws ZipException, IOException;
+	
 	Integer getPromedioUso(Map<Bicicleta, DatosBicicleta> bicicletasEnCsv);
 }
